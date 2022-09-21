3e4d2620a1ff38419b1370337424082f56291eba
Pedro Meira <petergamer8k@gmail.com>
2022-09-21 10:43:56 -0300

    feat: adding the home page file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Home } from './pages/Home'
+
 import './styles/main.css'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-neutral-800">
-      <h1 className="text-purple-600 text-7xl">Hello world</h1>
+    <div className="font-inter">
+      <Home />
     </div>
   )
 }
